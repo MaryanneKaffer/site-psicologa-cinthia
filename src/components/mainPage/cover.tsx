0499a7bb422d12a39ab890624a84d8e056474007
maryanne <kaffermaryanne@gmail.com>
2025-05-17 23:01:54 -0300

    vercel fix

--- src/components/mainPage/cover.tsx
+++ src/components/mainPage/cover.tsx
@@ -5,7 +5,7 @@ export default function Cover() {
         <section className="bg-[#8c947d] w-[100dvw] h-[100dvh] text-white sm:rounded-b-[100px] rounded-b-[50px] px-10 relative flex justify-center text-center overflow-hidden">
             <NavbarComponent />
             <h1 className="cursor-default lg:text-[13vh] text-[12vh] lg:leading-normal leading-[75px] font-ramillasItalic tracking-tight mt-[20vh]">Cinthia <span className="font-bold">Correia</span></h1>
-            <img draggable={false} src="/src/assets/coverPicture.png" alt="cover picture" className="sm:h-[80vh] lg:h-[85vh] object-cover absolute -bottom-[10vh] self-center" />
+            <img draggable={false} src="/assets/coverPicture.png" alt="cover picture" className="sm:h-[80vh] lg:h-[85vh] object-cover absolute -bottom-[10vh] self-center" />
         </section>
     )
 }
